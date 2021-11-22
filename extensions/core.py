eaf934a93a8ce567e9d6f2222326841e55473bed
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2021-11-22 22:55:03 +0000

    Disallow other users to use admin cog

--- extensions/core.py
+++ extensions/core.py
@@ -7,6 +7,13 @@ class Core(commands.Cog):
         await ctx.send('Pong!')
 
 class AdminCore(commands.Cog):
+    def cog_check(self, ctx):
+        return ctx.author.id == 419599149596672001
+
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send('Only my admins can use this command.')
+
     @commands.command(name='exit')
     async def exit_cmd(self, ctx):
         await ctx.send('Exiting.')
