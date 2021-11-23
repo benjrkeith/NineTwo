28651be47e146bdfdfd8da86688f6aa26325564f
Ben Keith <37029865+benjrkeith@users.noreply.github.com>
2021-11-23 10:26:09 +0000

    Use reply instead of send, update reply format

--- extensions/core.py
+++ extensions/core.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class Core(commands.Cog):
     @commands.command(name='ping')
     async def ping_cmd(self, ctx):
-        await ctx.send('Pong!')
+        await ctx.reply('Pong!')
 
 class AdminCore(commands.Cog):
     def cog_check(self, ctx):
@@ -12,11 +12,11 @@ class AdminCore(commands.Cog):
 
     async def cog_command_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            await ctx.send('Only my admins can use this command.')
+            await ctx.reply('Only my admins can use this command.')
 
     @commands.command(name='exit')
     async def exit_cmd(self, ctx):
-        await ctx.send('Exiting.')
+        await ctx.reply('Exiting.')
         await ctx.bot.exit()
 
 class ServerAdminCore(commands.Cog):
@@ -30,14 +30,14 @@ class ServerAdminCore(commands.Cog):
     async def prefix_cmd(self, ctx):
         prefix = await ctx.bot.get_prefix(ctx.message)
         fltr = filter(lambda p: str(ctx.bot.user.id) not in p, prefix)
-        await ctx.send(list(fltr))
+        await ctx.reply('\n'.join(list(fltr)))
 
     @prefix_cmd.command(name='new')
     async def prefix_new_cmd(self, ctx, *, prefix):
         config = ctx.bot.db.get_config(ctx.guild.id)
         config['prefixes'].append(prefix)
         config['in-sync'] = False
-        await ctx.reply('New prefix added.')
+        await ctx.reply(f'`{prefix}` added to prefixes.')
 
     @prefix_cmd.command(name='del')
     async def prefix_del_cmd(self, ctx, *, prefix):
@@ -46,7 +46,7 @@ class ServerAdminCore(commands.Cog):
         if config:
             config['prefixes'].remove(prefix)
             config['in-sync'] = False
-            await ctx.reply('Prefix removed.')
+            await ctx.reply(f'`{prefix}` removed from prefixes.')
 
 def setup(bot):
     bot.add_cog(Core())
